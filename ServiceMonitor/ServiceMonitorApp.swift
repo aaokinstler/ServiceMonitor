2d3d4201588b06e9bd98b9d9a2ef0381f28e7cb0
Anton Kinstler <antonkinstler@gmail.com>
2022-01-16 11:51:41 +0500

    Initial Commit

---
+++ ServiceMonitor/ServiceMonitorApp.swift
@@ -0,0 +1,20 @@
+//
+//  ServiceMonitorApp.swift
+//  ServiceMonitor
+//
+//  Created by Anton Kinstler on 16.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ServiceMonitorApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
